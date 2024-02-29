e278ae71bafe6dbfd52b706abdebd3311486b708
Prodwinson <124410666+Prodwinson@users.noreply.github.com>
2024-02-28 21:17:00 -0500

    Update scheduleTask.ps1

--- scheduleTask.ps1
+++ scheduleTask.ps1
@@ -1,3 +1,8 @@
+# Create an action for the Task 
 $action = New-ScheduledTaskAction -Execute ".\startVM.ps1"
+
+# Create a $trigger such as a time that will execute the $action
 $trigger = New-ScheduledTaskTrigger -Daily -At 9pm  
+
+# Create the schedule task following by a name, action, trigger, and a description
 Register-ScheduledTask -TaskName "Start-VM" -Action $action -Trigger $trigger -Description "Starts All Virtual Machines" -RunLevel Highest
